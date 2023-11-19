2ee33d4656e284fed7765e697a770c232e2cd1c2
erienkania <144706123+erienkania@users.noreply.github.com>
2023-11-19 21:08:53 +0700

    Add files via upload

---
+++ text.js
@@ -0,0 +1,7 @@
+alert('Selamat Datang Di Website TK Bustanul Aulad')
+window.onload = function() {
+  const menu_btn = Document.queryselector('.hamburger') ;
+  menu_btn.addEventlistener('click', function) () {
+    menu_btn.classlist.add('is-active')
+  }
+    });
